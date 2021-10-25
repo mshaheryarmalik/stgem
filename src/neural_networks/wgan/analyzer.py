eeeb1882a8e6fd3f364afcb681e7a65dee000231
Jarkko Peltomäki <jarkko.peltomaki@abo.fi>
2021-10-25 15:13:38 +0300

    Forgot to add WGAN neural networks.

---
+++ src/neural_networks/wgan/analyzer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class AnalyzerNetwork(nn.Module):
+  """
+  Define the neural network model for the WGAN analyzer.
+  """
+
+  def __init__(self, input_shape, neurons):
+    super(AnalyzerNetwork, self).__init__()
+
+    # The dimension of the input vector.
+    self.input_shape = input_shape
+    # Number of neurons per layer.
+    self.neurons = neurons
+
+    # We use three fully connected layers with self.neurons many neurons.
+    self.alayer1 = nn.Linear(self.input_shape, self.neurons)
+    self.alayer2 = nn.Linear(self.neurons, self.neurons)
+    self.alayer3 = nn.Linear(self.neurons, 1)
+
+  def forward(self, x):
+    x = F.relu(self.alayer1(x))
+    x = F.relu(self.alayer2(x))
+    x = torch.sigmoid(self.alayer3(x))
+
+    return x
---
+++ src/neural_networks/wgan/generator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GeneratorNetwork(nn.Module):
+  """
+  Define the neural network model for the WGAN generator.
+  """
+
+  def __init__(self, input_shape, output_shape, neurons):
+    super(GeneratorNetwork, self).__init__()
+
+    # The dimension of the input vector.
+    self.input_shape = input_shape
+    # The dimension of the output vector.
+    self.output_shape = output_shape
+    # Number of neurons per layer.
+    self.neurons = neurons
+
+    # We use three fully connected layers with self.neurons many neurons.
+    self.glayer1 = nn.Linear(self.input_shape, self.neurons)
+    self.glayer2 = nn.Linear(self.neurons, self.neurons)
+    self.glayer3 = nn.Linear(self.neurons, self.output_shape)
+
+  def forward(self, x):
+    x = F.relu(self.glayer1(x))
+    x = F.relu(self.glayer2(x))
+    x = torch.tanh(self.glayer3(x)) # Squash the values to [-1, 1].
+
+    return x
+
