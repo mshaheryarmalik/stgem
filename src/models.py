ba9fd903e6c2b93c411ab16e465c16664463fc03
Jarkko Peltomäki <jarkko.peltomaki@abo.fi>
2021-11-08 13:47:02 +0200

    Add more machine learning models for the analyzer.

--- src/models.py
+++ src/models.py
@@ -319,7 +319,7 @@ class WGAN(Model):
     self.modelG = neural_networks.wgan.generator.GeneratorNetwork(input_shape=self.noise_dim, output_shape=self.sut.ndimensions, neurons=self.neurons).to(self.device)
     self.modelC = neural_networks.wgan.critic.CriticNetwork(input_shape=self.sut.ndimensions, neurons=self.neurons).to(self.device)
     self.analyzer = Analyzer_NN(self.sut.ndimensions, self.device, self.logger)
-    #self.analyzer = Analyzer_AdaBoost(self.sut.ndimensions, self.device, self.logger)
+    #self.analyzer = Analyzer_RandomForest(self.sut.ndimensions, self.device, self.logger)
 
     # Optimizers.
     # TODO: figure out reasonable defaults and make configurable.
