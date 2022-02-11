6065601ce4aa11d41f49cc3c10273831334ea008
Ivan Porres <ivan.porres@abo.fi>
2022-02-11 16:32:32 +0200

    Use Job.setup_from_file

--- src/stgem.py
+++ src/stgem.py
@@ -8,16 +8,14 @@ from job import Job
 if __name__ == "__main__":
     jobs = []
     if len(sys.argv) > 1:
-        # Load test_jobs from the json files specified on the command line.
+        # Load jobs from the json files specified on the command line.
         for file_name in sys.argv[1:]:
             if not os.path.exists(file_name):
                 raise SystemExit("The file '{}' does not exist.".format(file_name))
-            with open(file_name) as f:
-                jobs.append(json.load(f))
+            jobs.append(Job().setup_from_file(file_name))
     else:
         print("usage: stgem.py job_description_file [... job_description_file_n]")
         raise SystemExit
 
     for job in jobs:
-        ajob = Job(job)
-        ajob.start()
+        job.start()
