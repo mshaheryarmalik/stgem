c27c22aa6ed39b2466f1d72c8ee5dfe1b5fa7c11
Ivan Porres <ivan.porres@abo.fi>
2022-02-21 15:26:31 +0200

    Example on how to use the new module loader for SUT

--- stgem/sut/__init__.py
+++ stgem/sut/__init__.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import importlib
-
 from .sut import SUT
 
+from stgem import load_stgem_module
 def loadSUT(name):
-    pcs = name.split(".")
-    try:
-        module = importlib.import_module("." + pcs[0] + ".sut", package="stgem.sut")
-    except ModuleNotFoundError:
-        raise Exception("The specified SUT module '{}' does not exist.".format(pcs[0]))
-    try:
-        the_class = getattr(module, pcs[1])
-    except AttributeError:
-        raise Exception("The specified SUT module '{}' does not have class '{}'.".format(pcs[0], pcs[1]))
-
-    return the_class
+   return load_stgem_module(name,"sut")
