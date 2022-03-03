48993daa8b59e243ccf48a0473c95dff3c92f8ed
fspencer <frankie.spencer@abo.fi>
2022-03-04 00:06:28 +0200

    Improve error reporting when loading modules dynamically: errors are reported from module and class level by traceback

--- stgem/__init__.py
+++ stgem/__init__.py
@@ -1,44 +1,53 @@
 import importlib
+import sys
+import traceback
+
 
 def load_stgem_class(name, namespace, module_path=None):
     # If there is no dot in the name, then we attempt to load the class
     # primarily from the appropriate file in the module path and secondarily
     # under stgem from the appropriate submodule. Otherwise we use the part
     # before the dot to indicate a submodule under stgem and load from there.
+
     if "." in name:
         pcs = name.split(".")
-        primary_module_name = "stgem." + namespace + "." + pcs[0] + "." + namespace
+        primary_module_name = "stgem.{}.{}.{}".format(namespace, pcs[0], namespace)
         secondary_module_name = None
         class_name = pcs[1]
     else:
         if module_path is None:
-            primary_module_name = "stgem." + namespace + "." + namespace
+            primary_module_name = "stgem.{}.{}".format(namespace, namespace)
             secondary_module_name = None
             class_name = name
         else:
-            primary_module_name = module_path + "." + namespace
-            secondary_module_name = "stgem." + namespace + "." + namespace
+            primary_module_name = "{}.{}".format(module_path, namespace)
+            secondary_module_name = "stgem.{}.{}".format(namespace, namespace)
             class_name = name
 
     module = None
+    traceback_record = str
     try:
         module_name = primary_module_name
         module = importlib.import_module(module_name)
     except ModuleNotFoundError:
+        traceback_record = traceback.format_exc()
         if secondary_module_name is not None:
             try:
                 module_name = secondary_module_name
                 module = importlib.import_module(module_name)
             except ModuleNotFoundError:
-                pass
+                traceback_record = traceback.format_exc()
 
     if module is None:
-        raise Exception("The module '{}' does not exist.".format(module_name))
+        print(traceback_record)
+        print("Error importing module '{}'.".format(module_name))
+        raise sys.exit()
 
     try:
         the_class = getattr(module, class_name)
     except:
-        raise Exception("The module '{}' does not have class '{}'.".format(module_name, class_name))
+        traceback.print_exc()
+        print("Error importing class '{}'.".format(module_name))
+        raise sys.exit()
 
     return the_class
-
