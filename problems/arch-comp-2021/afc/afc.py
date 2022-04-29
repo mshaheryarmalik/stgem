e7d76837daa9e9aeed6703bace3053d451ec70bc
Jarkko Peltomäki <jarkko.peltomaki@abo.fi>
2022-04-29 15:15:16 +0300

    Code and notebooks for AFC.

--- problems/arch-comp-2021/afc/afc.py
+++ problems/arch-comp-2021/afc/afc.py
@@ -1,84 +1,20 @@
 import os, sys
 
-import numpy as np
-
-import tltk_mtl as STL
-
 from stgem.generator import STGEM, Search, run_multiple_generators
 from stgem.budget import Budget
-from stgem.sut.matlab.sut import Matlab
+from stgem.objective import FalsifySTL
 from stgem.algorithm.random.algorithm import Random
 from stgem.algorithm.ogan.algorithm import OGAN
 from stgem.algorithm.ogan.model import OGAN_Model
 from stgem.algorithm.ogan.model_keras import OGANK_Model
-from stgem.algorithm.random.model import Uniform, LHS
 from stgem.algorithm.wogan.algorithm import WOGAN
 from stgem.algorithm.wogan.model import WOGAN_Model
-from stgem.objective import FalsifySTL
+from stgem.algorithm.random.model import Uniform, LHS
 from stgem.objective_selector import ObjectiveSelectorAll, ObjectiveSelectorMAB
 
+from util import build_specification
+
 mode = "stop_at_first_objective"
-scale = True
-specifications = ["AFC27", # AFC27, normal
-                  "AFC29"  # AFC29,AFC33 normal/power
-                 ]
-selected_specification = "AFC27"
-
-afc_mode = "normal" # normal/power
-if afc_mode == "normal":
-    throttle_range = [0, 61.2]
-elif afc_mode == "power":
-    throttle_range = [61.2, 81.2]
-
-def build_specification(selected_specification, asut):
-    # Notice that the output MODE is never used in the requirements.
-    sut_parameters = {"model_file": "problems/arch-comp-2021/afc/run_powertrain",
-                      "init_model_file": "problems/arch-comp-2021/afc/init_powertrain",
-                      "input_type": "piecewise constant signal",
-                      "output_type": "signal",
-                      "inputs": ["THROTTLE", "ENGINE"],
-                      "outputs": ["MU", "MODE"],
-                      "input_range": [throttle_range, [900, 1100]],
-                      "output_range": [[-1, 1], [0, 1]],
-                      "simulation_time": 50,
-                      "time_slices": [5, 50],
-                      "sampling_step": 0.5
-                     }
-
-    # We allow reusing the SUT for memory conservation (Matlab takes a lot of
-    # memory).
-    if asut is None:
-        asut = Matlab_Simulink(sut_parameters)
-
-    # Some ARCH-COMP specifications have requirements whose horizon is longer than
-    # the output signal for some reason. Thus strict horizon check needs to be
-    # disabled in some cases.
-    S = lambda var: STL.Signal(var, asut.variable_range(var) if scale else None)
-    if selected_specification == "AFC27":
-        beta = 0.008
-        # rise := (THROTTLE < 8.8) and (eventually[0,0.05](THROTTLE > 40.0))
-        L = FalsifySTL.StrictlyLessThan(1, 0, 0, 8.8, S("THROTTLE"))
-        R = STL.Finally(0, 0.05, FalsifySTL.StrictlyGreaterThan(1, 0, 0, 40, S("THROTTLE")))
-        rise = STL.And(L, R)
-        # fall := (THROTTLE > 40.0) and (eventually[0,0.05](THROTTLE < 8.8))
-        L = FalsifySTL.StrictlyGreaterThan(1, 0, 0, 40, S("THROTTLE"))
-        R = STL.Finally(0, 0.05, FalsifySTL.StrictlyLessThan(1, 0, 0, 8.8, S("THROTTLE")))
-        fall = STL.And(L, R)
-        # consequence := always[1,5](abs(MU) < beta)
-        consequence = STL.Global(1, 5, FalsifySTL.StrictlyLessThan(1, 0, 0, beta, STL.Abs(S("MU"))))
-        # always[11,50]( (rise or fall) implies (consequence)
-        specification = STL.Global(11, 50, STL.Implication(STL.Or(rise, fall), consequence))
-        
-        strict_horizon_check = False
-    elif selected_specification == "AFC29":
-        gamma = 0.007
-        # always[11,50]( abs(MU) < gamma )
-        specification = STL.Global(11, 50, FalsifySTL.StrictlyLessThan(1, 0, 0, gamma, STL.Abs(S("MU"))))
-        strict_horizon_check = True
-    else:
-        raise Exception("Unknown specification '{}'.".format(selected_specification))
-
-    return asut, specification, strict_horizon_check
 
 ogan_parameters = {"fitness_coef": 0.95,
                    "train_delay": 1,
@@ -153,14 +89,16 @@ if __name__ == "__main__":
     for i in range(N):
         asut, specification, strict_horizon_check = build_specification(selected_specification, asut)
         budget = Budget()
-        objectives = [FalsifySTL(specification=specification, epsilon=0.01, scale=scale, strict_horizon_check=strict_horizon_check)]
+        epsilon = 0.01
+        objectives = [FalsifySTL(specification=specification, epsilon=epsilon, scale=scale, strict_horizon_check=strict_horizon_check) for specification in specifications]
         objective_selector = ObjectiveSelectorAll()
         step_1 = Search(mode=mode,
-                        budget_threshold={"executions": 50},
-                        algorithm=Random(model_factory=(lambda: LHS(parameters={"samples": 50})))
+                        budget_threshold={"executions": 75},
+                        #algorithm=Random(model_factory=(lambda: LHS(parameters={"samples": 50})))
+                        algorithm=Random(model_factory=(lambda: Uniform()))
                        )      
         step_2 = Search(mode=mode,
-                        budget_threshold={"executions": 200},
+                        budget_threshold={"executions": 300},
                         #algorithm=WOGAN(model_factory=(lambda: WOGAN_Model()))
                         #algorithm=OGAN(model_factory=(lambda: OGANK_Model()))
                         algorithm=OGAN(model_factory=(lambda: OGAN_Model(ogan_model_parameters["convolution"])), parameters=ogan_parameters)
