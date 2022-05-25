2b80effc18da70fd0525e9a6a79100ac238fa2d9
Jarkko Peltomäki <jarkko.peltomaki@abo.fi>
2022-05-25 16:14:45 +0300

    Common code for all ARCH-COMP benchmarks. Support in AFC, AT, F16.

---
+++ problems/arch-comp-2021/run.py
@@ -0,0 +1,84 @@
+import importlib, os, sys
+
+import click
+
+from stgem.budget import Budget
+from stgem.generator import STGEM
+from stgem.experiment import Experiment
+from stgem.objective import FalsifySTL
+
+def get_generator_factory(description, sut_factory, objective_factory, objective_selector_factory, step_factory):
+    def generator_factory():
+        return STGEM(description=description,
+                     sut=sut_factory(),
+                     budget=Budget(),
+                     objectives=objective_factory(),
+                     objective_selector=objective_selector_factory(),
+                     steps=step_factory())
+
+    return generator_factory
+
+def get_seed_factory(init_seed=0):
+    def seed_generator(init_seed):
+        c = init_seed
+        while True:
+            yield c
+            c += 1
+
+    g = seed_generator(init_seed)
+    return lambda: next(g)
+
+def get_sut_objective_factory(benchmark_module, selected_specification, mode):
+    sut, _, _, _, _ = benchmark_module.build_specification(selected_specification, mode)
+
+    def sut_factory():
+        return sut
+
+    def objective_factory():
+        _, specifications, scale, strict_horizon_check, epsilon = benchmark_module.build_specification(selected_specification, mode, sut)
+        return [FalsifySTL(specification=specification, epsilon=epsilon, scale=scale, strict_horizon_check=strict_horizon_check) for specification in specifications]
+
+    return sut_factory, objective_factory
+
+benchmarks = ["AFC", "AT", "F16"]
+descriptions = {
+        "AFC": "Fuel Control of an Automotive Powertrain",
+        "AT":  "Automatic Transmission",
+        "F16": "Aircraft Ground Collision Avoidance System"
+}
+specifications = {
+        "AFC": ["AFC27", "AFC29"],
+        "AT":  ["AT1", "AT2", "AT51", "AT52", "AT53", "AT54", "AT6A", "AT6B", "AT6C", "AT6ABC", "ATX1", "ATX2"],
+        "F16": ["F16"]
+}
+
+@click.command()
+@click.argument("selected_benchmark", type=click.Choice(benchmarks, case_sensitive=False))
+@click.argument("selected_specification", type=str)
+@click.argument("mode", type=str, default="")
+@click.argument("n", type=int)
+@click.argument("init_seed", type=int)
+@click.argument("identifier", type=str, default="")
+def main(selected_benchmark, selected_specification, mode, n, init_seed, identifier):
+    N = n
+
+    if not selected_specification in specifications[selected_benchmark]:
+        raise Exception("No specification '{}' for benchmark {}.".format(selected_specification, selected_benchmark))
+
+    def callback(result):
+        path = os.path.join("..", "..", "output", selected_benchmark)
+        file_name = "{}{}_{}.pickle".format(selected_specification, "_" + identifier if identifier is not None else "", str(result.timestamp).replace(" ", "_"))
+        os.makedirs(path, exist_ok=True)
+        result.dump_to_file(os.path.join(path, file_name))
+
+    benchmark_module = importlib.import_module("{}.benchmark".format(selected_benchmark.lower()))
+
+    sut_factory, objective_factory = get_sut_objective_factory(benchmark_module, selected_specification, mode)
+    experiment = Experiment(N, get_generator_factory(descriptions[selected_benchmark], sut_factory, objective_factory, benchmark_module.get_objective_selector_factory(), benchmark_module.get_step_factory()), get_seed_factory(init_seed), result_callback=callback)
+
+    N_workers = 2
+    experiment.run(N_workers=N_workers, silent=False)
+
+if __name__ == "__main__":
+    main()
+
---
+++ problems/arch-comp-2021/run_hp.py
@@ -0,0 +1,74 @@
+import importlib, os, sys
+
+import click
+import numpy as np
+
+from stgem.algorithm.random.algorithm import Random
+from stgem.algorithm.random.model import Uniform, LHS
+from stgem.budget import Budget
+from stgem.experiment import Experiment
+from stgem.generator import STGEM, Search
+from stgem.objective import Minimize
+from stgem.objective_selector import ObjectiveSelectorAll
+from stgem.sut.hyper import HyperParameter, Range, Categorical
+
+from run import get_generator_factory, get_seed_factory, get_sut_objective_factory, benchmarks, specifications
+
+@click.command()
+@click.argument("selected_benchmark", type=click.Choice(benchmarks, case_sensitive=False))
+@click.argument("selected_specification", type=str)
+@click.argument("mode", type=str, default="")
+@click.argument("init_seed_experiments", type=int)
+@click.argument("seed_hp", type=int)
+def main(selected_benchmark, selected_specification, mode, init_seed_experiments, seed_hp):
+    if not selected_specification in specifications[selected_benchmark]:
+        raise Exception("No specification '{}' for benchmark {}.".format(selected_specification, selected_benchmark))
+
+    # We change the learning rates of the discriminator and the generator.
+    def f1(generator, value):
+        # Setup on generator has been called already, so the model objects
+        # exist. We edit their parameter dictionaries and resetup them.
+        for model in generator.steps[1].algorithm.models:
+            model.parameters["discriminator_lr"] = value
+            model.setup(model.search_space, model.device, model.logger)
+    def f2(generator, value):
+        # Similar to above.
+        for model in generator.steps[1].algorithm.models:
+            model.parameters["generator_lr"] = value
+            model.setup(model.search_space, model.device, model.logger)
+
+    hp_sut_parameters = {"hyperparameters": [[f1, Categorical([0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001])],
+                                             [f2, Categorical([0.5, 0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001])]],
+                         "mode":            "falsification_rate",
+                         "N_workers":       2}
+
+    benchmark_module = importlib.import_module("{}.benchmark".format(selected_benchmark.lower()))
+
+    def experiment_factory():
+        N = 25
+        sut_factory, objective_factory = get_sut_objective_factory(benchmark_module, selected_specification, mode)
+        return Experiment(N, get_generator_factory("", sut_factory, objective_factory, benchmark_module.get_objective_selector_factory(), benchmark_module.get_step_factory()), get_seed_factory(init_seed_experiments))
+
+    generator = STGEM(
+                      description="Hyperparameter search for benchmark {} and specification {}".format(selected_benchmark, selected_specification),
+                      sut=HyperParameter(experiment_factory, hp_sut_parameters),
+                      budget=Budget(),
+                      objectives=[Minimize(selected=[0], scale=False)],
+                      objective_selector=ObjectiveSelectorAll(),
+                      steps=[
+                          Search(budget_threshold={"executions": 64},
+                                 algorithm=Random(model_factory=(lambda: LHS(parameters={"samples": 64}))))
+                      ]
+    )
+
+    r = generator.run(seed=seed_hp)
+
+    X, _, Y = generator.test_repository.get()
+    Y = np.array(Y).reshape(-1)
+    for n in range(len(X)):
+        X2 = [hp_sut_parameters["hyperparameters"][i][1](x) for i, x in enumerate(X[n].inputs)]
+        print("{} -> {}".format(X2, 1 - Y[n]))
+
+if __name__ == "__main__":
+    main()
+
