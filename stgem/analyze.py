d348a748479af4462d07f6e2fe5edb4fbcc54ce4
Axel Björkqvist <axel.bjorkqvist@abo.fi>
2022-06-03 12:16:36 +0300

    Update stgem/analyze.py

--- stgem/analyze.py
+++ stgem/analyze.py
@@ -1,12 +1,14 @@
 from stgem.test_repository import TestRepository
 
 
-def test_repository_contour_plot(tr: TestRepository, show="sut_output"):
+def test_repository_contour_plot(tr: TestRepository, output_n=0, show="sut_output"):
     I, Osut , Oobjectives = tr.get()
     if show=="sut_output":
         O= Osut
     else:
         O= Oobjectives
+
+    TODO: select oput number output_n from O
 
     """
     TODO: plot a matplot countourplot of the variables I,O
