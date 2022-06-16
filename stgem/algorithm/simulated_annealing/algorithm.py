cb3953d2f6797cb8a928735d92c3541d5099f705
Jarkko Peltomäki <jarkko.peltomaki@abo.fi>
2022-06-16 12:58:09 +0300

    Minor touch-ups.
    Former-commit-id: e7efaef0401e0d1295269a82583e1672f0de8348

--- stgem/algorithm/simulated_annealing/algorithm.py
+++ stgem/algorithm/simulated_annealing/algorithm.py
@@ -4,9 +4,9 @@
 import numpy as np
 
 from stgem.algorithm import Algorithm
-import time
 
-class simulated_annealing(Algorithm):
+class SimulatedAnnealing(Algorithm):
+    """Implements simulated annealing."""
 
     default_parameters = {"radius": 0.1}
     
@@ -19,22 +19,18 @@ class simulated_annealing(Algorithm):
         pass
 
     def do_generate_next_test(self, active_outputs, test_repository, budget_remaining):
-
         #====Find the minimum between the variables of a vector (the objectives) that are in the active outputs====
         def find_min_vect(active_outputs,v):
-            temp=[]
-            for i in active_outputs:
-                temp.append(v[i]) 
-            minimum = min(temp)
-            return minimum  
+            return min(v[i] for i in active_outputs)
 
         #====Return the proba to accept a bad solution====
-        def find_proba(delta,T):
-            return np.exp(delta/ T)
+        def find_proba(delta, T):
+            print(delta, T, delta/T)
+            return np.exp(delta / T)
 
         #====Finds a random neighbor of the point x====    
         def neighbor(x):
-            neighbor = x + self.radius * np.random.uniform(-1, 1, size=self.search_space.input_dimension)
+            neighbor = np.array(x) + self.radius * np.random.uniform(-1, 1, size=self.search_space.input_dimension)
             np.clip(neighbor, -1, 1, out=neighbor)
             return neighbor
         
@@ -46,10 +42,19 @@ class simulated_annealing(Algorithm):
         invalid = 0 #number of invalid test
         T = init_T() #the temperature which is between 0 and 100 
 
+        #====Random for the first iteration====
+        if self.rounds == 0:
+            new_test = np.random.uniform(-1, 1, size=self.search_space.input_dimension)
+            self.prec = 0
+ 
+        #====Neighbor for the second iteration====    
+        if self.rounds == 1:
+            x,z,y =test_repository.get(-1)
+            new_test = neighbor(x.inputs)
+
         while True and (self.rounds >= 2):
-            self.rounds += 1
-            x1,z1,y1 =test_repository.get(-1) #values of the previous test (which is called the "next")
-            x2,z2,y2 =test_repository.get(-2 + self.prec) #values of the test that we get before the previous one (wich is called the "prev")
+            x1, _, y1 = test_repository.get(-1) #values of the previous test (which is called the "next")
+            x2, _, y2 = test_repository.get(-2 + self.prec) #values of the test that we get before the previous one (wich is called the "prev")
 
             f_Next = find_min_vect(active_outputs, y1) #the minimum objective of the previous test
             f_Prev = find_min_vect(active_outputs, y2) #the minimum objective that is kept (before the previous one)
@@ -61,40 +66,24 @@ class simulated_annealing(Algorithm):
                 #====The previous index is the new solution (so the index is -2)====
                 self.prec = 0
             else:
-                p1 = find_proba(f_Prev - f_Next,T)
+                p1 = find_proba(f_Prev - f_Next, T)
                 #====Accept less good solutions====
                 if p1 > np.random.random():
                     #====Calculate the next test from the less good solution====
                     new_test = neighbor(x1.inputs)
                     #====The previous index is the less good solution (so the index is -2)====
-                    self.prec=0
+                    self.prec = 0
                     #====If we don't accept the solution====
-                else :
+                else:
                     #====Calculate the next test from the previous solution====
                     new_test = neighbor(x2.inputs)
                     #====The previous value remains the same====
-                    self.prec-=1
+                    self.prec -= 1
             if self.search_space.is_valid(new_test) == 0:
                 invalid += 1
                 continue
             break
 
-        #====Neighbor for the second iteration====    
-        if (self.rounds==1):
-            x,z,y =test_repository.get(-1)
-            new_test = neighbor(x.inputs)
-            self.rounds += 1
-        
-        #====Random for the first iteration====
-        if (self.rounds==0):
-            new_test = np.random.uniform(-1, 1, size=self.search_space.input_dimension)
-            self.prec = 0
-            self.rounds += 1
-        
-
-        return np.array(new_test)
-
-
-
-
+        self.rounds += 1
+        return new_test
 
--- tests/test_mo3d_SA.py
+++ tests/test_mo3d_SA.py
@@ -1,22 +1,10 @@
-import math, os, unittest
+import os, unittest
 
-from stgem.budget import Budget
+from stgem.algorithm.simulated_annealing.algorithm import SimulatedAnnealing
 from stgem.generator import STGEM, Search
-from stgem.sut.python import PythonFunction
 from stgem.objective import Minimize
-from stgem.objective_selector import ObjectiveSelectorMAB
-from stgem.algorithm.simulated_annealing.algorithm import simulated_annealing
-from stgem.algorithm.random.algorithm import Random
-from stgem.algorithm.random.model import Uniform
-
-def myfunction(input: [[-15, 15], [-15, 15], [-15, 15]]) -> [[0, 350], [0, 350], [0, 350]]:
-    x1, x2, x3 = input[0], input[1], input[2]
-    h1 = 305 - 100 * (math.sin(x1 / 3) + math.sin(x2 / 3) + math.sin(x3 / 3))
-    h2 = 230 - 75 * (math.cos(x1 / 2.5 + 15) + math.cos(x2 / 2.5 + 15) + math.cos(x3 / 2.5 + 15))
-    h3 = (x1 - 7) ** 2 + (x2 - 7) ** 2 + (x3 - 7) ** 2 - (
-            math.cos((x1 - 7) / 2.75) + math.cos((x2 - 7) / 2.75) + math.cos((x3 - 7) / 2.75))
-
-    return [h1, h2, h3]
+from stgem.objective_selector import ObjectiveSelectorAll
+from stgem.sut.mo3d import MO3D
 
 class TestPython(unittest.TestCase):
     def test_SA(self):
@@ -24,18 +12,16 @@ class TestPython(unittest.TestCase):
 
         generator = STGEM(
             description="mo3d/SA",
-            sut=PythonFunction(function=myfunction),
-            budget=Budget(),
+            sut=MO3D(),
             objectives=[Minimize(selected=[0], scale=True),
                         Minimize(selected=[1], scale=True),
                         Minimize(selected=[2], scale=True)
                         ],
-            objective_selector=ObjectiveSelectorMAB(warm_up=5),
-            steps=[
-                            Search(budget_threshold={"executions": 1000},
-                                   mode=mode,
-                                   algorithm=simulated_annealing()
-                            )]
+            objective_selector=ObjectiveSelectorAll(),
+            steps=[Search(budget_threshold={"executions": 1000},
+                          mode=mode,
+                          algorithm=SimulatedAnnealing())
+                  ]
             )
         r = generator.run()
         file_name = "mo3d_python_SA_results.pickle"
