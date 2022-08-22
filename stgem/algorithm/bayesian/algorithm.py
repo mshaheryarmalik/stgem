c0b84763256ac2c9d98b9c0bd94c8f0ca75925ee
Jarkko Peltomäki <jarkko.peltomaki@abo.fi>
2022-08-22 10:17:49 +0300

    Cleanup.
    Former-commit-id: ed983c81ab9639df5b6c5ac11830432bfe08684a

--- stgem/algorithm/bayesian/algorithm.py
+++ stgem/algorithm/bayesian/algorithm.py
@@ -1,45 +1,36 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 import numpy as np
+import GPyOpt
 
 from stgem.algorithm import Algorithm
 
 class BayesOptSampler(Algorithm):
+    """Implements Bayesian optimization algorithm.
 
-    """Bayesian Optimization sampler : Defined only for continuous domains.
-    For discrete inputs define another sampler"""
-    def __init__(self):
-        super().__init__()
-        try:
-            import GPyOpt
-        except ModuleNotFoundError:
-            raise RuntimeError(
-                'BayesOptSampler requires GPyOpt to be installed')
-        self.bounds = []
-
-    def getVector(self,  active_outputs, test_repository):
-        import GPyOpt # do this here to avoid slow import when unused
-        X, _, Y = test_repository.get()
-        X = np.asarray([x.inputs for x in X])
-        minRHO = np.vstack([min([y[i] for i in active_outputs]) for y in Y])
-        BO = GPyOpt.methods.BayesianOptimization(
-                f=None, batch_size=1,
-                domain=self.bounds, X=X, Y=minRHO, normalize_Y=False)
-        sample = BO.suggest_next_locations()[0]
-        return np.asarray(tuple(sample))
+    Currently defined only for continuous domains."""
 
     def setup(self, search_space, device=None, logger=None):
         super().setup(search_space, device, logger)
-        self.rounds = 0
+
+        self.bounds = []
         for i in range(self.search_space.input_dimension):
-            self.bounds.append({'name':'x_'+str(i), 'type': 'continuous',
-                                'domain': (-1,1)})
+            self.bounds.append({"name": "x_{}".format(i),
+                                "type": "continuous",
+                                "domain": (-1, 1)})
 
     def do_train(self, active_outputs, test_repository, budget_remaining):
         pass
 
     def do_generate_next_test(self, active_outputs, test_repository, budget_remaining):
-        self.test = self.getVector(active_outputs, test_repository)
-        self.rounds += 1
-        return self.test
+        X, _, Y = test_repository.get()
+        X = np.asarray([x.inputs for x in X])
+        Y = np.array([min(y[i] for i in active_outputs) for y in Y]).reshape(X.shape[0], 1)
+        BO = GPyOpt.methods.BayesianOptimization(
+                f=None,
+                batch_size=1,
+                domain=self.bounds,
+                X=X,
+                Y=Y,
+                normalize_Y=False)
+        test = BO.suggest_next_locations()[0]
+        return np.asarray(test)
+
--- tests/test_bayesian-opt.py
+++ tests/test_bayesian-opt.py
@@ -1,23 +1,12 @@
 import os, unittest, math
 
-from stgem.sut.python import PythonFunction
 from stgem.algorithm.bayesian.algorithm import BayesOptSampler
+from stgem.algorithm.random.algorithm import Random
+from stgem.algorithm.random.model import Uniform
 from stgem.generator import STGEM, Search
 from stgem.objective import Minimize
 from stgem.objective_selector import ObjectiveSelectorAll
-from stgem.algorithm.random.model import Uniform
-from stgem.algorithm.random.algorithm import Random
-
-def myfunction(input: [[-15, 15], [-15, 15], [-15, 15]]) -> [[0, 350], [0, 350], [0, 350]]:
-    x1, x2, x3 = input[0], input[1], input[2]
-    h1 = 305 - 100 * (math.sin(x1 / 3) + math.sin(x2 / 3) + math.sin(x3 / 3))
-    h2 = 230 - 75 * (math.cos(x1 / 2.5 + 15) + math.cos(x2 / 2.5 + 15) + math.cos(x3 / 2.5 + 15))
-    h3 = (x1 - 7) ** 2 + (x2 - 7) ** 2 + (x3 - 7) ** 2 - (
-            math.cos((x1 - 7) / 2.75) + math.cos((x2 - 7) / 2.75) + math.cos((x3 - 7) / 2.75))
-
-    return [h1, h2, h3]
-
-# NOTE!: requires verifai and GPyOpt package to run
+from stgem.sut.mo3d import MO3D
 
 class TestBayesOptSampler(unittest.TestCase):
     def test_python(self):
@@ -25,7 +14,7 @@ class TestBayesOptSampler(unittest.TestCase):
 
         generator = STGEM(
             description="bayesianOpt",
-            sut=PythonFunction(function=myfunction),
+            sut=MO3D(),
             objectives=[Minimize(selected=[0], scale=True),
                         Minimize(selected=[1], scale=True),
                         Minimize(selected=[2], scale=True)
@@ -36,14 +25,12 @@ class TestBayesOptSampler(unittest.TestCase):
                        mode=mode,
                        algorithm=Random(model_factory=(lambda: Uniform()))),
                 Search(budget_threshold={"executions": 5},
-                        mode=mode,
-                        algorithm=BayesOptSampler())
+                       mode=mode,
+                       algorithm=BayesOptSampler())
                   ]
             )
         r = generator.run()
-        file_name = "bayesianOpt_results.pickle"
-        r.dump_to_file(file_name)
-        os.remove(file_name)
 
 if __name__ == "__main__":
     unittest.main()
+
