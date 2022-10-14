d0d699a045fd49eb54c7b91f6276e0079d552d4a
Jarkko Peltomäki <japeltom@abo.fi>
2022-10-14 11:19:26 +0300

    Fix the previous changes.

--- stgem/budget.py
+++ stgem/budget.py
@@ -3,11 +3,9 @@ import math, time
 class Budget:
 
     def __init__(self):
-        self.budget_ids = ["executions", "execution_time", "generation_time"]
+        self.budget_ids = ["executions", "execution_time", "generation_time", "training_time", "wall_time"]
         self.budget_values = {name:0 for name in self.budget_ids}
-        self.budget_values["wall_time"] = 0
         self.budget_threshold = {name:math.inf for name in self.budget_ids}
-        self.budget_threshold["wall_time"] = math.inf
         self.wall_time_start = -1
 
     def update_threshold(self, budget_threshold):
@@ -23,7 +21,7 @@ class Budget:
         return min(self.used().values())
 
     def used(self):
-        result = {name:max(0, 1 - self.budget_values[name]/self.budget_threshold[name]) for name in self.budget_ids}
+        result = {name:max(0, 1 - self.budget_values[name]/self.budget_threshold[name]) for name in self.budget_ids if name != "wall_time"}
         result["wall_time"] = max(0, 1 - (time.perf_counter() - self.wall_time_start)/self.budget_threshold["wall_time"])
 
         return result
@@ -32,12 +30,11 @@ class Budget:
         """Consume a budget, i.e., add the given value (default 1) to the used
         budget corresponding to the budget id.
 
-        It is assumed that this method is not called on consumable budgets like
-        wall time. No checks are made to prevent this.
+        It is assumed that this method is not called on nonconsumable budgets
+        like wall time. No checks are made to prevent this.
 
         If the budget id is not recognized, nothing is done."""
 
         if budget_id in self.budget_ids:
             value = args[0] if len(args) > 0 else 1
             self.budget_values[budget_id] += value
-
