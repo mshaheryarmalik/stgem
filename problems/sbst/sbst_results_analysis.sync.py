c925b10b88b8b48a7c727c48bf2f8c3d9a90fd4e
Jarkko Peltomäki <jarkko.peltomaki@abo.fi>
2022-11-01 09:32:43 +0200

    Notebook code for computing diversity measures.

--- problems/sbst/sbst_results_analysis.sync.py
+++ problems/sbst/sbst_results_analysis.sync.py
@@ -11,8 +11,7 @@
 #     language: python
 #     name: python3
 # ---
-import os, sys
-from math import degrees
+import itertools, math, os, sys
 
 sys.path.append(os.path.join("..", ".."))
 sys.path.append(os.path.join("..", "..", "notebooks"))
@@ -22,33 +21,111 @@ from common import *
 # # Domain-Specific Code
 
 # %%
-def steering_sd(sut_output):
-    """Computes the standard deviation of the steering angle from a SUTOutput
-    object. This is a behavioral diversity measure used in the SBST 2022
-    report."""
+def move_road(P, x0, y0):
+    """Moves the sequence of points P in such a way that the initial point is
+    at (x0, y0) and the initial direction is up."""
 
-    return np.std(sut_output.outputs[3])
+    X = np.array(P)
 
-def direction_coverage(sut_input, n_bins=36):
-    """Compute the coverage of the the road directions. That is, compute the
-    angles between two consecutive road points and place the angles into bins
-    (default 36 bins, i.e., bins cover 10 degrees) and return the proportion of
-    bins covered. The angle is defined as the angle to the vertical axis."""
+    if len(X) == 1:
+        Q = np.array([x0, y0])
+    else:
+        # Find the translation angle.
+        angle = math.pi / 2 - math.atan2(X[1,1] - X[0,1], X[1,0] - X[0,0])
+        # Translation vector to origin.
+        o_x = -X[0,0]
+        o_y = -X[0,1]
 
-    points = sut_input.input_denormalized
+        Q = np.empty_like(X)
+        for n in range(len(X)):
+            # Map to origin for rotation.
+            x = X[n,0] + o_x
+            y = X[n,1] + o_y
+            # Rotate the point and translate the resulting point back.
+            Q[n,0] = math.cos(angle) * x - math.sin(angle) * y + x0
+            Q[n,1] = math.sin(angle) * x + math.cos(angle) * y + y0
 
-    # Compute the angles.
-    angles = []
-    for i in range(0, points.shape[1], 2):
-        vector = np.array([points[0,i+1] - points[0,i], points[1,i+1] - points[1,i]])
-        angle = degrees(np.arccos( vector[1] / np.linalg.norm(vector) ))
-        angles.append(angle)
+    if isinstance(P, list):
+        return Q.tolist()
+    else:
+        return Q
 
-    # Place into bins.
-    bins = np.linspace(0.0, 360.0, num=n_bins + 1)
-    covered_bins = set(np.digitize(angles, bins))
+def steering_sd(test_repository):
+    """Compute the standard deviation of the steering angles for each test in
+    the test suite. This is a behavioral diversity measure used in the SBST
+    2022 report."""
 
-    return len(covered_bins) / len(bins)
+    _, Z, _ = test_repository.get()
+
+    data = [np.std(sut_output.outputs[3]) for sut_output in Z]
+
+    return data
+
+def direction_coverage(test_repository, bins=36):
+    """Compute the coverage of road directions of the test suite. That is, for
+    each road, compute the angles between two consecutive road points and place
+    the angles into bins (default 36 bins, i.e., bins cover 10 degrees) and
+    return the proportion of bins covered. The angle is defined as the angle to
+    the vertical axis. This is a structural diversity measure used in the SBST
+    2022 report."""
+
+    def road_coverage(sut_input, bins):
+        points = sut_input.input_denormalized
+
+        # Compute the angles.
+        angles = []
+        for i in range(0, points.shape[1] - 1, 2):
+            vector = np.array([points[0,i+1] - points[0,i], points[1,i+1] - points[1,i]])
+            angle = math.degrees(np.arccos( vector[1] / np.linalg.norm(vector) ))
+            angles.append(angle)
+
+        # Place into bins.
+        bins = np.linspace(0.0, 360.0, num=bins + 1)
+        covered_bins = set(np.digitize(angles, bins))
+
+        return len(covered_bins) / len(bins)
+
+    X, _, _ = test_repository.get()
+
+    data = [road_coverage(sut_input, bins) for sut_input in X]
+
+    return data
+
+def euclidean_diversity(test_repository, adjusted_points, threshold):
+    """Computes the median of the pairwise Euclidean distances of the
+    failed tests of the given test suite after the roads of the test suite
+    have been normalized to have a common number of points and turned into
+    angles."""
+
+    def adjust_road_signal(road_points, points):
+        """Adjusts an interpolated road to have the specified number of points."""
+
+        # Notice that the road points are given a signal of plane points of shape
+        # (2, N).
+        road_points = np.transpose(road_points).reshape(-1, 2)
+        idx = np.round(np.linspace(0, len(road_points) - 1, points)).astype(int)
+        adjusted = road_points[idx]
+        return move_road(adjusted, 0, 0)
+
+    X, _, Y = test_repository.get()
+    Y = np.array(Y).reshape(-1)
+
+    converted_failed_tests = []
+    for n in range(len(Y)):
+        if Y[n] >= threshold: continue
+        # Adjust the road to have a common number of points.
+        adjusted_road = adjust_road_signal(X[n].input_denormalized, adjusted_points)
+        # Convert the adjusted road into a sequence of angles.
+        diff = np.diff(adjusted_road, axis=0)
+        angles = np.arctan2(diff[:,0], diff[:,1])
+        converted_failed_tests.append(angles)
+
+    # Compute pairwise Euclidean distances for the tests.
+    euclidean_distances = [np.linalg.norm(t1 - t2) for t1, t2 in itertools.combinations(converted_failed_tests, 2)]
+    # Compute the median Euclidean distance.
+    median = np.median(euclidean_distances)
+
+    return median
 
 # %% [markdown]
 # # Load Experiments
@@ -58,7 +135,7 @@ output_path_base = os.path.join("..", "..", "output")
 
 # Replica prefixes for collecting the experiments.
 replica_prefixes = {
-    "SBST": ["SBST", "1000"]
+    "SBST": ["OLD"]
 }
 
 experiments = loadExperiments(output_path_base, ["SBST"], replica_prefixes)
@@ -89,24 +166,31 @@ for identifier in experiments:
 
 own_boxplot(data, list(experiments), title="Number of Failed Tests", ylabel="Number of Failed Tests")
 
+# %% [markdown]
+# # Test Suite Diversity
+
 # %%
-result = experiments["1000"][0]
-tr = result.test_repository
-X, Z, Y = tr.get()
-Y = np.array(Y).reshape(-1)
+def test_suite_euclidean_diversity(experiment, threshold):
+    # We have determined this number experimentally.
+    adjusted_points = 75
+    return [euclidean_diversity(result.test_repository, adjusted_points, threshold) for result in experiment]
 
-idx = []
-for i in range(len(Y)):
-    if Y[i] <= 0.05:
-        idx.append(i)
+# %%
+# We have determined this number experimentally.
+adjusted_points = 75
 
-for i in idx:
-    sut_output = Z[i]
-    dl = sut_output.outputs[1]
-    dr = sut_output.outputs[2]
-    j = np.argmin(dl)
-    k = np.argmin(dr)
-    print(i)
-    print("  L: {}, {}, {}".format(j, dl[j], dr[j]))
-    print("  R: {}, {}, {}".format(k, dl[k], dr[k]))
+diversity_values = [[euclidean_diversity(result.test_repository, adjusted_points, failure_threshold) for result in experiments[identifier]] for identifier in experiments]
+
+#print(diversity_values)
+own_boxplot(diversity_values, list(experiments), title="Test Suite Diversity Euclidean", ylabel="Test Suite Diversity Euclidean")
+
+# %%
+diversity_values = [[steering_sd(result.test_repository) for result in experiments[identifier]] for identifier in experiments]
+
+print(diversity_values)
+
+# %%
+diversity_values = [[direction_coverage(result.test_repository) for result in experiments[identifier]] for identifier in experiments]
+
+print(diversity_values)
 
